05d55f66599bc46d7c492e616d23ef4e1dcf9eff
GBTC440014UR <GBTC440014UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-03-25 18:09:04 -0400

    Initial commit

---
+++ src/main/java/com/bysue/springboot_203/Springboot203Application.java
@@ -0,0 +1,13 @@
+package com.bysue.springboot_203;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot203Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot203Application.class, args);
+	}
+
+}
